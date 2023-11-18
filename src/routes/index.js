b479477634fa740123c53c8b7f7760ba26262ebd
MacBook Pro <macbookpro@MacBook-Pro-cua-MacBook.local>
2023-11-18 21:00:36 +0700

    update router, default layout

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privarteRoutes = [];
+
+export { publicRoutes, privarteRoutes };
